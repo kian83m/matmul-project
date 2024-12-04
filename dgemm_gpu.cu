ec291889d1810838d9dd6cc6e37a5f707e7b4623
Terry-planetariummars <terrywang330@gmail.com>
2024-12-03 18:45:14 -0800

    some major order switch

--- dgemm_gpu.cu
+++ dgemm_gpu.cu
@@ -15,10 +15,15 @@ void square_dgemm_kernel(const int M, const double *A0, const double *B0, double
     const int y = threadIdx.y + blockDim.y * blockIdx.y;
     // C[x][y] = 0;
     if(x < M && y < M){
-        C0[x * M + y] = 0.0;
+        // C0[x * M + y] = 0.0;
+        // // calculating C[x][y] as inner product of row x of A and col y of B (all zero-indexing)
+        // for(int k = 0; k < M; k++){
+        //     C0[x * M + y] += A0[x * M + k] * B0[y + k * M];
+        // }
+        C0[y * M + x] = 0.0;
         // calculating C[x][y] as inner product of row x of A and col y of B (all zero-indexing)
         for(int k = 0; k < M; k++){
-            C0[x * M + y] += A0[x * M + k] * B0[y + k * M];
+            C0[y * M + x] += A0[k * M + x] * B0[k + y * M];
         }
     }
 }
@@ -35,6 +40,7 @@ void square_dgemm(const int M, const double *A0, const double *B0, double *C0)
     dim3 numBlocks(CEIL_DIV(M, blocksize), CEIL_DIV(M, blocksize));
     dim3 threadsPerBlock(blocksize, blocksize);
     square_dgemm_kernel<<<numBlocks, threadsPerBlock>>>(M, A, B, C);
+    cudaDeviceSynchronize();
     cudaMemcpy(C0, C, M * M * sizeof(double), cudaMemcpyDeviceToHost);
     cudaFree(A);
     cudaFree(B);
